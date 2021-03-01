342a548fc53f9566d0898e5595b31a319fde5820
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-03-01 11:08:22 +0300

    feat:task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+      return error;
+    }else{
+      let newArrResult = [];
+  
+      arr.forEach((e,i) => {
+        if(e === '--double-next'){
+          newArrResult.push(arr[i+1]);
+        } else if(e === '--double-prev'){
+          newArrResult.push(newArrResult[newArrResult.length-1]);
+        } else if(arr[i+1] === '--discard-prev'){
+          newArrResult.push(undefined);
+        } else if(arr[i-1] === '--discard-next'){
+          newArrResult.push(undefined);
+        } else {
+          newArrResult.push(e);
+        }
+      })
+  
+      return newArrResult.filter(element => {
+          if(element !== '--discard-prev' && element !== '--discard-next' && element !== undefined){
+            return true;
+          }else{
+            return false;
+          }
+        });
+    }
+
 };
