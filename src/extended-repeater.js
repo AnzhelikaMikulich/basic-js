3da6dde33b11e0d1545caf2ed7391951485851ed
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-03-01 11:16:10 +0300

    feat:add task extanded repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = [];
+
+  if (options.addition !== undefined) {
+    if (options.additionRepeatTimes) {
+      for (i = 0; i < options.additionRepeatTimes; i++) {
+        addition.push(String(options.addition));
+      }
+    } else {
+      addition.push(String(options.addition));
+    }
+  }
+  if (options.additionSeparator) {
+    addition = addition.join(options.additionSeparator);
+  } else {
+    addition = addition.join("|");
+  }
+
+  let repetitionArray = [];
+
+  if (options.repeatTimes) {
+    for (i = 0; i < options.repeatTimes; i++) {
+      repetitionArray.push(String(str) + addition);
+    }
+  } else {
+    repetitionArray.push(String(str) + addition);
+  }
+
+  if (options.separator) {
+    return repetitionArray.join(options.separator);
+  } else {
+    return repetitionArray.join("+");
+  }
 };
-  
