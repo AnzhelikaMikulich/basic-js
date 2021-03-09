1892abb11265f647486257753f16718643f78e5c
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-03-09 16:33:14 +0300

    init: start tasc simple - chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  result: [],
+	getLength() {
+		return this.result.length;
+	},
+	addLink(value) {
+		this.result.push(`( ${value} )`);
+		return this;
+	},
+	removeLink(position) {
+		if (position <= 0 || isNaN(position) || position % 1 !== 0) {
+			this.result = [];
+			throw new Error();
+		} else {
+			this.result.splice(position - 1, 1);
+			return this;
+		}
+	},
+	reverseChain() {
+		this.result.reverse();
+		return this;
+	},
+	finishChain() {
+		let chain = this.result.join('~~');
+		this.result = [];
+		return chain;
+	}
 };
 
 module.exports = chainMaker;
